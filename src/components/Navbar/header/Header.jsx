692db03ac764530d63cfcfc0557992d8d4f98283
nitinyadav12492 <ny2102417@gmail.com>
2026-01-25 22:00:31 +0530

    Update

--- src/components/Navbar/header/Header.jsx
+++ src/components/Navbar/header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 import"./Header.css"
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className='header'>
          <div className="header-contents">
             <h2>Order your favourite food here</h2>
             <p>Choose from a diverse menu featuring a delectabe array of dishes crafted with th finest ingredients and culinary expertise . our mission is to satisy yur craving amd elevate your dinner experience , one delicious meal at a time </p>
-            <button>View Menu</button>
+            <button onClick={() => navigate("/card")}>
+      Explore Menu
+    </button>
          </div>
 
     </div>
